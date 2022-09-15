aca62907794bee78d06cae175078c28e16e16fa3
Darrik <darrikwh@gmail.com>
2022-09-15 15:35:48 -0700

    feat: add wait

---
+++ bin/wait/Wait.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+#include "Wait.h"
+
+Wait::Wait(int argc, char **argv)
+    : POSIXApplication(argc, argv)
+{
+    parser().setDescription("Wait for a PID to finish its process");
+    parser().registerPositional("PID ID", "The PID to wait for");
+}
+
+Wait::~Wait()
+{
+}
+
+Wait::Result Wait::exec()
+{
+    ERROR("Alej hates darrik and rita!");
+}
